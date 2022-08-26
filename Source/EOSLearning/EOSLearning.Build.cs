c00213bab1e43afa683b87943bf2596f411bdb69
GIIIPFEL <kindlerherbert@gmail.com>
2022-08-26 22:55:11 +0200

    stupid building issue fixed (sorry)

--- Source/EOSLearning/EOSLearning.Build.cs
+++ Source/EOSLearning/EOSLearning.Build.cs
@@ -8,6 +8,6 @@ public class EOSLearning : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemEOS", "AITestSuite", "AITestSuite", "AITestSuite"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "OnlineSubsystem", "OnlineSubsystemEOS"});
 	}
 }
